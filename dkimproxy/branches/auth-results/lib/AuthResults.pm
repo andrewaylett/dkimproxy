f7e661bbf99de553103cc3aefb3c2f023b5cccd7
jlong <jlong@f38efd27-133c-0410-a3cc-a5f95e9cf04f>
2007-11-16 21:54:36 +0000

    more work on AuthResults.pm

--- dkimproxy/branches/auth-results/lib/AuthResults.pm
+++ dkimproxy/branches/auth-results/lib/AuthResults.pm
@@ -13,6 +13,7 @@ sub new
 		use Sys::Hostname;
 		$self->host( hostname() );
 	}
+	$self->{Results} = [];
 
 	return $self;
 }
@@ -37,16 +38,42 @@ sub host
 {
 	my $self = shift;
 	@_ and
-		$self->{host} = shift;
-	return $self->{host};
+		$self->{Host} = shift;
+	return $self->{Host};
+}
+
+sub results
+{
+	my $self = shift;
+	return @{$self->{Results}};
+}
+
+sub append_result
+{
+	my $self = shift;
+	my %args = @_;
+	push @{$self->{Results}}, \%args;
+}
+
+sub result_as_string
+{
+	my $self = shift;
+	my $result_spec = shift;
+
+	return $result_spec->{Method} . "=" . $result_spec->{Result}
+		. ($result_spec->{Comment} ? (" (" . $result_spec->{Comment} . ")") : "")
+		. join("",
+			map { " " . $_->[0] . "=" . $_->[1] }
+				@{$result_spec->{Specifiers}}
+			);
 }
 
 sub version
 {
 	my $self = shift;
 	@_ and
-		$self->{version} = shift;
-	return $self->{version};
+		$self->{Version} = shift;
+	return $self->{Version};
 }
 
 1;
--- dkimproxy/branches/auth-results/lib/test_auth_results.pl
+++ dkimproxy/branches/auth-results/lib/test_auth_results.pl
@@ -8,3 +8,25 @@ use AuthResults;
 
 my $auth_results = AuthResults->new;
 print $auth_results->as_string . "\n";
+
+$auth_results->host("host.example.com");
+print $auth_results->as_string . "\n";
+
+$auth_results->append_result(
+	Method => "dkim",
+	Result => "pass",
+	Comment => "with flying colors",
+	Specifiers => [ [ "header.i" => "foo\@example.com" ] ],
+	);
+print $auth_results->as_string . "\n";
+
+push @{$auth_results->{Results}->[0]->{Specifiers}},
+	[ "header.sender" => "foo\@example.com" ];
+print $auth_results->as_string . "\n";
+
+$auth_results->append_result(
+	Method => "spf",
+	Result => "softfail",
+	Specifiers => [ [ "smtp.from" => "nobody\@dev.null" ] ],
+	);
+print $auth_results->as_string . "\n";
